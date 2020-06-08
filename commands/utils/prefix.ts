c0fff30e3fc49c43d063e08fe6554035c35670f3
suyash singh <suyashtnt@gmail.com>
2020-06-08 11:23:03 +0200

    removed old json method

--- commands/utils/prefix.ts
+++ commands/utils/prefix.ts
@@ -1,22 +1,18 @@
 
 const { Command } = require('discord-akairo');
-const config = require('../../config.json')
-var guildConf = require('../../storages/guildConf.json');
-const fs = require('fs');
 import * as Discord from 'discord.js'
 class PrefixCommand extends Command {
     constructor() {
         super('prefix', {
             aliases: ['prefix', 'pf'],
             description: {
-                text: 'Changes the prefix, can only be used every 30 minutes',
+                text: 'Changes the prefix',
                 usage: `prefix [new prefix]`
             },
-            cooldown: 1800000,
             ratelimit: 1,
             args: [
                 {
-                    id: "newprefix",
+                    id: "prefix",
                     default: null,
                 },
             ],
@@ -35,7 +31,6 @@ class PrefixCommand extends Command {
             .setDescription(
                 `
                 The new prefix is \`${args.prefix}\`
-                Keep note that he bot restarts on change, until https://github.com/discord-akairo/discord-akairo/pull/125 gets merged it'll be like this.
                 `
 
             )
--- index.ts
+++ index.ts
@@ -5,9 +5,6 @@ const { AkairoClient, CommandHandler } = require('discord-akairo');
 const MongooseProvider = require('akairo-mongoose');
 const mongoose = require('mongoose');
 const config = require("./config.json");
-var guildConf = require("./storages/guildConf.json");
-const fs = require("fs");
-import * as Discord from "discord.js";
 const model = require("./models/Prefixes");
 class MyClient extends AkairoClient {
     constructor() {
@@ -22,7 +19,7 @@ class MyClient extends AkairoClient {
                     // The third param is the default.
                     return this.settings.get(message.guild.id, 'prefix', 'e.');
                 }
-                return config.token;
+                return 'e.';
             },
             defaultCooldown: 1000,
         });
@@ -59,27 +56,4 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity("e.help");
 });
-client.on("guildCreate", (guild: Discord.Guild) => { // If the Bot was added on a server, proceed
-    if (!guildConf[guild.id]) { // If the guild's id is not on the GUILDCONF File, proceed
-        guildConf[guild.id] = {
-            prefix: config.prefix,
-        };
-    }
-    fs.writeFile(
-        "./storages/guildConf.json",
-        JSON.stringify(guildConf, null, 2),
-        (err: any) => {
-            if (err) console.log(err);
-        },
-    );
-});
-client.on("guildDelete", (guild: any) => { // If the Bot was removed on a server, proceed
-    delete guildConf[guild.id]; // Deletes the Guild ID and Prefix
-    fs.writeFile(
-        "./storages/guildConf.json",
-        JSON.stringify(guildConf, null, 2),
-        (err: any) => {
-            if (err) console.log(err);
-        },
-    );
-});
+
