fb1d3b681d80594e5d5775f2b5e52accc3462701
suyash singh <suyashtnt@gmail.com>
2020-06-08 10:23:08 +0200

    updated stuff

--- commands/info/help.ts
+++ commands/info/help.ts
@@ -1,56 +1,63 @@
-
-import * as Akario from 'discord-akairo'
-import * as Discord from 'discord.js'
-const config = require('../../config.json')
-var guildConf = require('../../storages/guildConf.json');
+import * as Akario from "discord-akairo";
+import * as Discord from "discord.js";
+const config = require("../../config.json");
+var guildConf = require("../../storages/guildConf.json");
 class PingCommand extends Akario.Command {
-    constructor() {
-        super('help', {
-            aliases: ['h', 'help'],
-            args: [
-                {
-                    id: 'commandname',
-                    type: 'command'
-                }
-            ],
-            description: {
-                text: 'Displays this message',
-                usage: `e.help`
-            },
+  constructor() {
+    super("help", {
+      aliases: ["h", "help"],
+      args: [
+        {
+          id: "commandname",
+          type: "command",
+        },
+      ],
+      description: {
+        text: "Displays this message",
+        usage: `e.help`,
+      },
+    });
+  }
 
-        });
-    }
+  exec(message: Discord.Message, args: any) {
+    message.channel.send(`Helping...`).then((msg) => {
+      if (!args.commandname) {
+        const _ = new Discord.MessageEmbed()
+          .setTitle("Here's your help!")
+          .setDescription(
+            ``,
+          )
+          .setColor(`#00FF00`);
+        this.handler.modules
+          .each((command) =>
+            _.setDescription(
+              _.description +
+                `\`${
+                  guildConf[message.guild!.id].prefix
+                }${command.description.usage}\` - ${command.description.text} - aliases: ${
+                  command.aliases.join(", ")
+                }\n\n`,
+            )
+          );
+        message.channel.send(_);
+      } else {
+        console.log(args.commandname);
 
-    exec(message: Discord.Message, args: any) {
-
-
-        message.channel.send(`Helping...`).then(() => {
-            if (!args.commandname) {
-                const _ = new Discord.MessageEmbed()
-                    .setTitle("Here's your help!")
-                    .setDescription(
-                        ``
-                    )
-
-                    .setColor(`#00FF00`);
-                this.handler.modules
-                    .each(command => _.setDescription(_.description + `\`${guildConf[message.guild!.id].prefix}${command.description.usage}\` - ${command.description.text} - aliases: ${command.aliases.join(', ')}\n\n`));
-                message.channel.send(_);
-            } else {
-                const _ = new Discord.MessageEmbed()
-                    .setTitle("Here's your help!")
-                    .setDescription(
-                        `${args.commandname.name}`
-                    )
-
-                    .setColor(`#00FF00`);
-                message.channel.send(_);
-            }
-
-        })
-
-
-    }
+        const _ = new Discord.MessageEmbed()
+          .setTitle(`\`${args.commandname.aliases}\``)
+          .setDescription(
+            `
+                        ${args.commandname.description.text}\n
+                        usage
+                        ${args.commandname.description.usage}
+                        `,
+          )
+          .setColor(`#00FF00`);
+        message.channel.send(_);
+        msg.delete();
+      }
+    });
+  }
 }
 
 module.exports = PingCommand;
--- index.ts
+++ index.ts
@@ -1,41 +1,49 @@
 const tslib_1 = require("tslib");
 const Akairo = require("discord-akairo");
-const config = require('./config.json');
-var guildConf = require('./storages/guildConf.json');
-const fs = require('fs');
-import * as Discord from 'discord.js';
+const config = require("./config.json");
+var guildConf = require("./storages/guildConf.json");
+const fs = require("fs");
+import * as Discord from "discord.js";
 class MyClient extends Akairo.AkairoClient {
-    constructor() {
-        super({}, {});
-        this.commandHandler = new Akairo.CommandHandler(this, {
-            directory: './commands/',
-            prefix: (message: any) => {
-                return guildConf[message.guild.id].prefix;
-            },
-            defaultCooldown: 1000
-        });
-        this.commandHandler.loadAll();
-    }
+  constructor() {
+    super({}, {});
+    this.commandHandler = new Akairo.CommandHandler(this, {
+      directory: "./commands/",
+      prefix: (message: any) => {
+        return guildConf[message.guild.id].prefix;
+      },
+      defaultCooldown: 1000,
+    });
+    this.commandHandler.loadAll();
+  }
 }
 const client = new MyClient();
 client.login(config.token);
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("e.help");
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("e.help");
 });
-client.on('guildCreate', (guild: Discord.Guild) => { // If the Bot was added on a server, proceed
-    if (!guildConf[guild.id]) { // If the guild's id is not on the GUILDCONF File, proceed
-        guildConf[guild.id] = {
-            prefix: config.prefix
-        }
-    }
-    fs.writeFile('./storages/guildConf.json', JSON.stringify(guildConf, null, 2), (err: any) => {
-        if (err) console.log(err)
-    })
+client.on("guildCreate", (guild: Discord.Guild) => { // If the Bot was added on a server, proceed
+  if (!guildConf[guild.id]) { // If the guild's id is not on the GUILDCONF File, proceed
+    guildConf[guild.id] = {
+      prefix: config.prefix,
+    };
+  }
+  fs.writeFile(
+    "./storages/guildConf.json",
+    JSON.stringify(guildConf, null, 2),
+    (err: any) => {
+      if (err) console.log(err);
+    },
+  );
 });
-client.on('guildDelete', (guild: any) => { // If the Bot was removed on a server, proceed
-    delete guildConf[guild.id]; // Deletes the Guild ID and Prefix
-    fs.writeFile('./storages/guildConf.json', JSON.stringify(guildConf, null, 2), (err: any) => {
-        if (err) console.log(err)
-    })
+client.on("guildDelete", (guild: any) => { // If the Bot was removed on a server, proceed
+  delete guildConf[guild.id]; // Deletes the Guild ID and Prefix
+  fs.writeFile(
+    "./storages/guildConf.json",
+    JSON.stringify(guildConf, null, 2),
+    (err: any) => {
+      if (err) console.log(err);
+    },
+  );
 });
