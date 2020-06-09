3d683c774ceb898097024a8adf693f606cd16e3b
KrazyMiner001 <57949410+KrazyMiner001@users.noreply.github.com>
2020-06-09 11:40:50 +0200

    Update invite.ts

--- commands/utils/invite.ts
+++ commands/utils/invite.ts
@@ -15,7 +15,7 @@ class InviteCommand extends Command {
   }
 
   exec(message: Discord.Message) {
-    message.channel.send('https://discordapp.com/oauth2/authorize?client_id=718471280914858015&scope=bot&permissions=1074088000');
+    message.channel.send('https://discordapp.com/oauth2/authorize?client_id=718471280914858015&scope=bot&permissions=1074261072');
   }
 }
 module.exports = InviteCommand;
