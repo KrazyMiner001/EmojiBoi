c7616e989cdaf4c92a8a37f8acf9fe51c20c6a56
suyash singh <suyashtnt@gmail.com>
2020-06-10 16:42:43 +0200

    did stuff

--- commands/Emojis/enlargeEmoji.ts
+++ commands/Emojis/enlargeEmoji.ts
@@ -1,5 +1,9 @@
-const { Command } = require('discord-akairo');
+const {
+    Command
+} = require('discord-akairo');
 import * as Discord from 'discord.js'
+var fs = require('fs'),
+    cloudconvert = new (require('cloudconvert'))(process.env.cckey);
 
 class EnlargeCommand extends Command {
     constructor() {
@@ -9,20 +13,35 @@ class EnlargeCommand extends Command {
                 text: 'Enlarges a emoji',
                 usage: `enlarge [emoji to enlarge]`
             },
-            args: [
-                {
-                    id: 'em',
-                    type: 'emoji',
-                    default: null
-                },
-            ],
+            args: [{
+                id: 'em',
+                type: 'emoji',
+                default: null
+            }, ],
             clientPermissions: ['MANAGE_EMOJIS'],
             userPermissions: ['MANAGE_EMOJIS'],
         });
     }
     exec(message: Discord.Message, args: any) {
-        const attachment = new Discord.MessageAttachment(`${args.em.url}`);
+        let url = args.em.url;
+        console.log(url);
+
+        if (args.em.url.endsWith('svg')) {
+            fs.createReadStream(url)
+                .pipe(cloudconvert.convert({
+                    "inputformat": "svg",
+                    "outputformat": "png",
+                    "input": "upload"
+                }))
+                .pipe(fs.createWriteStream('file.png'));
+            url = 'file.png'
+
+        }
+        const attachment = new Discord.MessageAttachment(`${url}`);
         message.channel.send(attachment);
+        if (args.em.url.endsWith('svg')) {
+            fs.unlink('file.png', console.error)
+        }
     }
 }
 
